98fa13f76c0725b03f0fe89a3b67bea97f378235
zl <zl@qq.com>
2021-06-20 18:23:15 +0800

    second commit

--- src/main/java/com/zl/Test/GitTest.java
+++ src/main/java/com/zl/Test/GitTest.java
@@ -3,5 +3,6 @@ package com.zl.Test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
