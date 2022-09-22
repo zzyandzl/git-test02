dd8382d6c0d374ec946a74578198338dd850c7e4
zl <1939564674@qq.com>
2022-09-22 19:29:57 +0800

    提交第2次

--- src/main/java/com/zl/Test/Test01.java
+++ src/main/java/com/zl/Test/Test01.java
@@ -6,5 +6,9 @@ public class Test01 {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date);
+
+        System.out.println("second commit");
+
+
     }
 }
