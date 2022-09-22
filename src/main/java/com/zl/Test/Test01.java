80399d6c2a77d0b40114667dcda1f0e3f8f761d1
zl <1939564674@qq.com>
2022-09-22 19:28:03 +0800

    提交第一次

---
+++ src/main/java/com/zl/Test/Test01.java
@@ -0,0 +1,10 @@
+package com.zl.Test;
+
+import java.util.Date;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
